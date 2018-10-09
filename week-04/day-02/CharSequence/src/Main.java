30baf74faa2f303d6eaddc91b7e9ecd1327e640a
Sam'Bo <herrerasam88@gmail.com>
2018-10-09 11:31:57 -0400

    All done for today

--- week-04/day-02/CharSequence/src/Main.java
+++ week-04/day-02/CharSequence/src/Main.java
@@ -4,12 +4,10 @@ public class Main {
         Gnirts g = new Gnirts("example");
         g.length();
         g.charAt(1);
-        g.subSequence(1, 4);
 
         Shifters s = new Shifters("example", 2);
         s.length();
         s.charAt(0);
-        System.out.println(s.charAt(0));
-        System.out.println(s.subSequence(1, 3));
+        System.out.println(s.charAt(0));;
     }
 }
