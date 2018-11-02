a1e0fed421d2544891131ace5bb09a0e069ef52f
Sam'Bo <herrerasam88@gmail.com>
2018-11-02 08:50:08 -0400

    Exercise 1 complete on Streams

---
+++ week-06/Streams/Exercises/src/Exercise1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Exercise1 {
+    //Write a Stream Expression to get the even numbers from the following array:
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14));
+
+        numbers.stream()
+        .filter(p -> p % 2 == 0).
+                forEach(System.out::println);
+
+
+    }
+}
