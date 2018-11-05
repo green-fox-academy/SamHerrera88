e01f89002508f5998ce3a2feaaff9e1dca06037e
Sam'Bo <herrerasam88@gmail.com>
2018-11-05 10:29:02 -0500

    RESTGREETCOUNTER

--- springstart/src/main/java/Com/GreenFox/controllers/HelloRESTController.java
+++ springstart/src/main/java/Com/GreenFox/controllers/HelloRESTController.java
@@ -1,14 +1,16 @@
-package controllers;
+package Com.GreenFox.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class HelloRESTController {
 
     @RequestMapping(value = "/greeting")
-    public Greeting greeting() {
-        return new Greeting(1,"Hello World!");
+    public Greeting greeting(@RequestParam("name") String name,AtomicLong atomicLong) {
+        return new Greeting(1,"Hello," + name);
     }
 }
